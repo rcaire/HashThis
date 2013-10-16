b82f9e4258ed5728e3c6b20637abed4122cf3a0a
Ramiro Caire <ramiro.caire@gmail.com>
2013-10-15 23:39:30 -0300

    First version of HashThis

--- hashthis.py
+++ hashthis.py
@@ -9,39 +9,39 @@ import os,hashlib
 def en_md5(txt):
 	     """Function to convert plain text to MD5 hash"""
 	     text = hashlib.md5(txt).hexdigest()
-	     print "-" * 95
+	     print "-" * 89
 	     print "[*] Hash MD5 output: " +  text
 	     print "[*] Hash length: " + str(len(text)) + " characters - (128-bits hash value)"
-	     print "-" * 95
+	     print "-" * 89
 
 def en_sha1(txt):
 	     """Function to convert plain text to SHA-1 hash"""
 	     text = hashlib.sha1(txt).hexdigest()
-	     print "-" * 95
+	     print "-" * 89
 	     print "[*] Hash SHA-1 output: " +  text
 	     print "[*] Hash length: " + str(len(text)) + " characters - (160-bits hash value)"
-	     print "-" * 95
+	     print "-" * 89
 
 def en_sha256(txt):
              """Function to convert plain text to SHA-256 hash"""
              text = hashlib.sha256(txt).hexdigest()
-             print "-" * 95
+             print "-" * 89
              print "[*] Hash SHA-256 output: " +  text
              print "[*] Hash length: " + str(len(text)) + " characters - (256-bits hash value)"
-             print "-" * 95
+             print "-" * 89
 
 
 def en_sha512(txt):
 	     """Function to convert plain text to SHA-512 hash"""
              text = hashlib.sha512(txt).hexdigest()
-             print "-" * 95
+             print "-" * 89
              print "[*] Hash SHA-512 output: " +  text
              print "[*] Hash length: " + str(len(text)) + " characters - (512-bits hash value)"
-             print "-" * 95
+             print "-" * 89
 
 def main():
 	os.system('clear')
-	print ("-" * 40 + "[ HashThis v0.1 ]" + "-" * 40)
+	print ("-" * 36 + "[ HashThis v0.1 ]" + "-" * 36)
 	ht = raw_input("[+] Type text to be hashed: ")
 	print ""
 	print "1- MD5"
